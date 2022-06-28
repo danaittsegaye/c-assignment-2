e4325f5dddaad962455487a4b1f2631e6b75f8bc
danaittsegaye <107928545+danaittsegaye@users.noreply.github.com>
2022-06-28 21:31:48 +0300

    initializing

---
+++ assignment 2/assignment 2/Program.cs
@@ -0,0 +1,27 @@
+﻿//fibonacci series upto 100
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 0, n2 = 1, sum;
+            sum = 0;
+            Console.Write(" " + n1 + "  " + n2 + "  ");
+            sum = n1 + n2;
+            while (sum <= 100)
+            {
+                Console.Write(sum + "  ");
+                n1 = n2;
+                n2 = sum;
+                sum = n1 + n2;
+            }
+        }
+    }
+}
